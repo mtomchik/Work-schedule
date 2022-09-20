65a741f62852020d51443fd7f3b0202aa9bac354
Mtomchik <matthew.tomchik@yahoo.com>
2022-09-19 20:53:19 -0600

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Work-schedule
+Description: This is a work scheldule organizer that should be able to update in real time and save tqasks between uses using local storage.
+
+Visuals:
+
+"Screenshot"
+
+Usage: You can use this to keep track and organize tasks
+
+Support email Matt at Matthew.tomchik@yahoo.com
+
+Authors and acknowledgment Me and my instructr Jay and his TAs Rob and Maria.
--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,42 @@
+var saveButton = document.querySelector(".fa fa-save")
+var input = document.querySelector(".form-control")
+var currentTime =  moment().format("H");
+var scheduledTime = document.querySelector(".input-group-text").innerHTML
+
 var today = moment();
-moment().format("dddd, MMMM Do YYYY, h:mm: a"); 
-var update = function() {
+moment().format("dddd, MMMM Do YYYY, h:mm: a");
+var update = function () {
     document.getElementById("currentDay")
-    .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+        .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');     
 }
 setInterval(update, 1000);
 
-var timeColor = function (){
-    if (document.getElementById("input-group-text").innerHTML === document.getElementById("currentDay").innerHTML) {}
+function userInput() {
+    var task = input.value;
+    localStorage.setItem("task", JSON.stringify(task));
+    console.log(task);
 }
+function renderMessage(){
+    var task = JSON.parse(localStorage.getItem("input"));
+    if (task !== null) {
+        document.querySelector(".form-control").textContent = task
+    }
+} 
+function colorCode(){
+    // for (var i = 0; i < input.length; i++ ) {
+    //     input.setAttribute("style", "background-color: gray")
+    // }
+    if (currentTime < scheduledTime) {
+        input.setAttribute("style", "background-color: green");
+    } else if (currentTime = scheduledTime) {
+        input.setAttribute("style", "background-color: red");
+    } else {
+        input.setAttribute("style", "background-color: gray");
+    }}
+    console.log(scheduledTime);
+    console.log(currentTime);
+//     if (currentTime = scheduledTime){
+//         input[0].setAttribute("style", "background color: red");
+//     }else if (currentTime>)
+// }
+saveButton.addEventListener("click", userInput())
